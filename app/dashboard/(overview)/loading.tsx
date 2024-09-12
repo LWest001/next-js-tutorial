f5c5451159f81f81df058e8ba591dd07749df81a
Leo Westebbe <lw@1x-nat-vl931-172-30-162-20.wireless.umass.edu>
2024-09-12 15:40:50 -0400

    chapter 12, creating an entry

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardSkeleton from "@/app/ui/skeletons";
+
+const Loading = function () {
+  return <DashboardSkeleton />;
+};
+
+export default Loading;
--- app/ui/invoices/pagination.tsx
+++ app/ui/invoices/pagination.tsx
@@ -4,36 +4,50 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 import clsx from 'clsx';
 import Link from 'next/link';
 import { generatePagination } from '@/app/lib/utils';
+import { usePathname, useSearchParams } from "next/navigation";
 
 export default function Pagination({ totalPages }: { totalPages: number }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+  const currentPage = Number(searchParams.get("page")) || 1;
+
+  function createPageUrl(page: number) {
+    const params = new URLSearchParams(searchParams);
+    if (page <= 1) {
+      params.delete("page");
+    } else {
+      params.set("page", page.toString());
+    }
+    return `${pathname}?${params.toString()}`;
+  }
   // NOTE: Uncomment this code in Chapter 11
 
-  // const allPages = generatePagination(currentPage, totalPages);
+  const allPages = generatePagination(currentPage, totalPages);
 
   return (
     <>
       {/*  NOTE: Uncomment this code in Chapter 11 */}
 
-      {/* <div className="inline-flex">
+      <div className="inline-flex">
         <PaginationArrow
           direction="left"
-          href={createPageURL(currentPage - 1)}
+          href={createPageUrl(currentPage - 1)}
           isDisabled={currentPage <= 1}
         />
 
         <div className="flex -space-x-px">
           {allPages.map((page, index) => {
-            let position: 'first' | 'last' | 'single' | 'middle' | undefined;
+            let position: "first" | "last" | "single" | "middle" | undefined;
 
-            if (index === 0) position = 'first';
-            if (index === allPages.length - 1) position = 'last';
-            if (allPages.length === 1) position = 'single';
-            if (page === '...') position = 'middle';
+            if (index === 0) position = "first";
+            if (index === allPages.length - 1) position = "last";
+            if (allPages.length === 1) position = "single";
+            if (page === "...") position = "middle";
 
             return (
               <PaginationNumber
                 key={page}
-                href={createPageURL(page)}
+                href={createPageUrl(page)}
                 page={page}
                 position={position}
                 isActive={currentPage === page}
@@ -44,10 +58,10 @@ export default function Pagination({ totalPages }: { totalPages: number }) {
 
         <PaginationArrow
           direction="right"
-          href={createPageURL(currentPage + 1)}
+          href={createPageUrl(currentPage + 1)}
           isDisabled={currentPage >= totalPages}
         />
-      </div> */}
+      </div>
     </>
   );
 }
